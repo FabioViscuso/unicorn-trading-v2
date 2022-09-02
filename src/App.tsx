bb7c07f0c0594ec12a22d8e45e6589883184719e
Fabio Viscuso <viscuso.fabio@outlook.it>
2022-09-02 13:08:45 +0200

    feat: added initial code for persistent login

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,20 @@ import { Dashboard } from './pages/Dashboard';
 import { Protected } from './components/navigation/Protected';
 import { Header } from './components/navigation/Header';
 import { ParticlesContainer } from './components/ui/ParticlesContainer';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { loginActions } from './store/slices/login';
 
 function App() {
+  // run one time to check for existing auth data
+  const dispatch = useDispatch();
+  useEffect(() => {
+    const localAuthData: { username: string, token: string } = JSON.parse(localStorage.getItem('authUser') as string)
+    if (localAuthData && typeof localAuthData !== 'undefined') {
+      dispatch(loginActions.login({ username: localAuthData.username, token: localAuthData.token }))
+    }
+  })
+
   return (
     <div className="font-['Quicksand'] flex md:flex-row flex-col ">
       <ParticlesContainer />
